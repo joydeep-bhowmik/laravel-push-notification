a62707438503de94e6b4a959f6c7033a6d2ec165
joydeep-bhowmik <bhowmikj9432@gmail.com>
2024-09-20 16:49:19 +0530

    not publishing some file problem fixed

--- src/Providers/FcmServiceProvider.php
+++ src/Providers/FcmServiceProvider.php
@@ -13,18 +13,17 @@ class FcmServiceProvider extends ServiceProvider
 
         // Publish all resources under one tag
         $this->publishes([
-            __DIR__ . '/config/fcm.php' => config_path('fcm.php'),
-            __DIR__ . '/resources/js/firebase-messaging-sw.js' => public_path('firebase-messaging-sw.js'),
-            __DIR__ . '/resources/js/fcm.js' => public_path('js/fcm.js'),
-            __DIR__ . '/resources/views/components/push-notification-switch.blade.php' => resource_path('views/components/push-notification-switch.blade.php'),
-            __DIR__ . '/migrations/create_user_devices_table.php' => database_path('migrations/' . date('Y_m_d_His') . '_create_user_devices_table.php'), // Publish migration with timestamp
-
-            __DIR__ . '/storage/app/firebase-auth.js' => public_path('js/firebase-auth.js'), // Publish firebase-auth.js
+            __DIR__ . '/../config/fcm.php' => config_path('fcm.php'),
+            __DIR__ . '/../resources/js/firebase-messaging-sw.js' => public_path('firebase-messaging-sw.js'),
+            __DIR__ . '/../resources/js/fcm.js' => resource_path('js/fcm.js'),
+            __DIR__ . '/../resources/views/components/push-notification-switch.blade.php' => resource_path('views/components/push-notification-switch.blade.php'),
+            __DIR__ . '/../migrations/create_user_devices_table.php' => database_path('migrations/' . date('Y_m_d_His') . '_create_user_devices_table.php'),
+            __DIR__ . '/../storage/app/firebase-auth.js' => storage_path('app/firebase-auth.js'),
         ], 'fcm-all');
 
 
         $this->publishes([
-            __DIR__ . '/Models/UserDevice.php' => app_path('Models/UserDevice.php')
+            __DIR__ . '/../Models/UserDevice.php' => app_path('Models/UserDevice.php')
         ], 'fcm-tokens-model');
     }
 
